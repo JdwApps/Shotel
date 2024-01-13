053a30276931df7156e4661bc5d15e7d71b43d90
Jeejee2064 <j.de.watteville@gmail.com>
2024-01-13 15:45:30 -0500

    bro

--- app/Leasures/page.js
+++ app/Leasures/page.js
@@ -2,36 +2,42 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import BGSlider from "../components/BGSlider";
 import Title from "../components/Title";
-import BocasMap from "../components/BocasMap";
 import ImaBeach from "../components/ImaBeach";
 import ImaHikes from "../components/ImaHikes";
 import ImaSnorkel from "../components/ImaSnorkel";
 import ImaSurf from "../components/ImaSurf";
 import Separator from "../components/Separator";
 import SeparatorB from "../components/SeparatorB";
+import dynamic from 'next/dynamic';
 
 
 const Leasures = () => {
+    const DynamicBocasMap = dynamic(
+        () => import('../components/BocasMap'),
+        { ssr: false }
+       );
+    
+
     return (
         <div className="bg-gray-800">
             <NavBar />
-            <BGSlider title='Leasures' 
-      images = {['plage1.jpg', 'plage2.jpg', 'plage3.jpg', 'plage4.jpg']}
-      />
+            <BGSlider title='Leasures'
+                images={['plage1.jpg', 'plage2.jpg', 'plage3.jpg', 'plage4.jpg']}
+            />
             <div className='py-12'>
-            <Title text='Activities'/>
-            <ImaBeach/>
-            <Separator/>
-            <ImaHikes/>
-            <SeparatorB/>
-            <ImaSnorkel/>
-            <Separator/>
-            <ImaSurf/>
-            <SeparatorB/>
-            
-            <BocasMap/>
+                <Title text='Activities' />
+                <ImaBeach />
+                <Separator />
+                <ImaHikes />
+                <SeparatorB />
+                <ImaSnorkel />
+                <Separator />
+                <ImaSurf />
+                <SeparatorB />
+
+                <DynamicBocasMap />
             </div>
-    
+
             <Footer />
         </div>
     );
